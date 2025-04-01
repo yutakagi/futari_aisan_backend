107ddc1f23417b94561ae3fda4ed41c6b9f1dfe6
takagiyuta <nngggy@gmail.com>
2025-04-01 23:34:43 +0900

    git再デプロイ

--- db.py
+++ db.py
@@ -22,7 +22,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
 
-# SSL証明書のパス（Azure上のKuduにアップしたパス）
+# SSL証明書のパス（Azure上のKuduにアップしたパスに修正）
 SSL_CERT_PATH = '/home/site/wwwroot/certs/ca-cert.pem'
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" 
