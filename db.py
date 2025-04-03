84cc977bc93a9b18ef9ab63b2f792dcd3e4c8c12
Miyu Sanematsu <miyu.saneco@gmail.com>
2025-04-03 17:05:02 +0900

    Add conversation chain feature

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+### db.py ###
 # 役割：データベースの接続設定やテーブル定義を行う
 # SQL Alchemyを利用してDBとの接続やモデルを定義している（今回はユーザーからの生の解答テキストと、それを要約したテキストを保存するテーブル）
 from sqlalchemy import Column, Integer, String, Text
@@ -26,14 +27,22 @@ DB_NAME = os.getenv('DB_NAME')
 ssl_cert = '/home/site/wwwroot/certs/ca-cert.pem'
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" 
+
+# # ローカルで動かすとき ##
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, 
+#     echo=True
+# )
+
+# Azure上にデプロイするとき ##
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
     echo=True,
     connect_args={
         "ssl":{
             "ca":ssl_cert
+            }
         }
-    }
     )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
