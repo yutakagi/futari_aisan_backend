1fad0ef2285858b07f90713916c4ceb725f2eb2b
MitsuruMe <mitsurunano@gmail.com>
2025-04-14 22:11:23 +0900

    db.pyの内容を復元（255888a）

--- db.py
+++ db.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, Integer, String, Text, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+### db.py ###
+# 役割：データベースの接続設定やテーブル定義を行う
+# SQL Alchemyを利用してDBとの接続やモデルを定義している（今回はユーザーからの生の解答テキストと、それを要約したテキストを保存するテーブル）
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -7,29 +12,40 @@ from dotenv import load_dotenv
 # 環境変数の読み込み
 load_dotenv()
 base_path = Path(__file__).parent
+env_path = base_path / '.env'
 
+
+# データベース接続情報
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-# SSL証明書パス（ローカル用）
-ssl_cert = str(base_path / 'DigiCertGlobalRootCA.crt.pem')
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# SSL証明書のパス（ローカルで動かすとき）
+# ssl_cert = str(base_path / 'DigiCertGlobalRootCA.crt.pem')
 
-# 環境判定
-DEPLOY_ENV = os.getenv("DEPLOY_ENV", "local")
+# SSL証明書のパス（Azure上のKuduにアップしたパスに修正）
+ssl_cert = '/home/site/wwwroot/certs/ca-cert.pem'
 
-if DEPLOY_ENV == "azure":
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        echo=True,
-        connect_args={"ssl": {"ca": ssl_cert}}
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" 
+
+# # ローカルで動かすとき ##
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, 
+#     echo=True
+# )
+
+#Azure上にデプロイするとき ##
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    echo=True,
+    connect_args={
+        "ssl":{
+            "ca":ssl_cert
+            }
+        }
     )
-else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
